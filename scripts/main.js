603d923bbdc59bed793934be3bcfcbb37715c80e
Sarah Pickett <pickett.sarah.louise@gmail.com>
2019-10-17 22:12:06 -0400

    charts are here

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 Highcharts.chart('container', {
 
     title: {
-        text: 'US AID Spending, 2010 - 2017'
+        text: 'US AID Spending, 2010 - 2016'
     },
 
     subtitle: {
-        text: 'Source: thesolarfoundation.com'
+        text: 'Source: explorer.usaid.gov'
     },
 
     yAxis: {
@@ -29,10 +29,9 @@ Highcharts.chart('container', {
     },
 
     series: [{
-
-        name: 'Money in billions',
-        data: [49, 50, 46, 43, 49, 50, 49]
-    }],
+        name: 'Total Obligations',
+        data: [49, 50, 46, 43, 49, 50, 59, ]
+    }, ],
 
     responsive: {
         rules: [{
@@ -49,4 +48,119 @@ Highcharts.chart('container', {
         }]
     }
 
+});
+
+Highcharts.chart('container2', {
+    chart: {
+        type: 'packedbubble',
+        height: '100%'
+    },
+    title: {
+        text: 'Sector Foreign Aid Spending (2017)'
+    },
+    tooltip: {
+        useHTML: true,
+        pointFormat: '<b>{point.name}:</b> {point.value} billion'
+    },
+    plotOptions: {
+        packedbubble: {
+            minSize: '30%',
+            maxSize: '120%',
+            zMin: 0,
+            zMax: 20,
+            layoutAlgorithm: {
+                splitSeries: false,
+                gravitationalConstant: 0.02
+            },
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}',
+                filter: {
+                    property: 'y',
+                    operator: '>',
+                    value: 20
+                },
+                style: {
+                    color: 'black',
+                    textOutline: 'none',
+                    fontWeight: 'normal'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Conflict, Peace & Security',
+        data: [{
+            name: 'Conflict, Peace & Security',
+            value: 15
+        },
+      ]
+    }, {
+        name: 'Emergency Response',
+        data: [{
+            name: "Emergency Response",
+            value: 8.3
+        },
+     ]
+    },{
+        name: 'HIV/Aids',
+        data: [{
+            name: "HIV/Aids",
+            value: 6.1
+        },
+      ]
+    }, {
+        name: 'Agriculture',
+        data: [{
+            name: "Agriculture",
+            value: 1.2
+        },
+      ]
+    },{
+        name: 'Basic Health',
+        data: [{
+            name: "Basic Health",
+            value: 1.6
+        },
+      ]
+    }, {
+        name: 'Government and Civil Society',
+        data: [{
+            name: "Government and Civil Society",
+            value: 7.6
+        },
+
+      ]
+    },{
+        name: 'General Environmental Protection',
+        data: [{
+            name: "General Environmental Protection",
+            value: 7.6
+        },
+
+      ]
+    }, {
+        name: 'Basic Educationn',
+        data: [{
+            name: "Basic Education",
+            value: 1.2
+        },
+      ]
+    },
+    {
+        name: 'Other',
+        data: [{
+            name: "Other",
+            value: 6.5
+        },
+      ]
+    },
+    {
+        name: 'Operating Expenses',
+        data: [{
+            name: "Operating Expenses",
+            value: 7.2
+        },
+    ]
+    }]
 });
